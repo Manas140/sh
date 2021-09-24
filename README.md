472151c1fb6986817ca0e04edd67c33522ac39c9
manas140 <manasbhosale.14@gmail.com>
2021-09-24 12:24:25 +0000

    posix rewrite

--- README.md
+++ README.md
@@ -3,15 +3,29 @@ Collection Of Sh Scripts I Made
 
 **Preview**
 
-<p align="center">
-  <img src="preview.png">
+<p>
+  <img width=200 src="preview/block.png">
+  <img width=200 src="preview/col.png">
+  <img width=200 src="preview/ext.png"> 
+  <img width=200 src="preview/rain.png">
+  <img width=200 src="preview/sing.png">  
+  <img width=200 src="preview/trash.png">
 </p>
 
+
 **Programs Included**
+
 ```
-  col             output terminal colors.
-  ext             extract compressed files.
+  block           print colored blocks.
+  col             show color scheme.
+  ext             extract file.
   rain            colorful rain of text.
-  sing            colorful echo/printf.
-  pluck           get color pixel on screen.
+  sing            print colored text.
+  trash           print colored text.
+```
+
+**Install**
+```
+  git clone https://github.com/manas140/sh.git && cd sh
+  sudo ./install i
 ```
--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-scripts=$(ls --hide 'LICENSE' --hide '*.*')
-red="\033[1;31m"
-green="\033[1;32m"
-case $1 in
-  i|install)
-    for script in $scripts; do
-      cp -r $script /usr/local/bin/. && printf "$green[*] $script installed\n"
-    done;;
-  u|uninstall) 
-    for script in $scripts; do
-      rm -rf /usr/local/bin/$script && printf "$red[-] $script uninstalled\n"
-    done;;
-  *) printf "$green""i|install          install scripts.\nu|uninstall        uninstall scripts.\n";;
+cr="\033[1;31m"
+cg="\033[1;32m"
+cb="\033[1;34m"
+
+printf "${cr}"
+case $1 in 
+  u) for script in $(ls src/); do 
+    rm -r /usr/local/bin/$script && printf "${cg}[*] $script Uninstalled\n" || printf "${cr}[-] There Was An Error While Uninstalling $script\n"; 
+  done;;
+  i) for script in $(ls src/); do
+    cp src/$script /usr/local/bin/$script && printf "${cg}[*] $script Installed\n" || printf "${cr}[-] There Was An Error While Installing $script\n";
+  done;;
+  up) sh install.sh u
+    printf "${cr}"
+    git pull && sh install.sh i || printf "${cr}Please Resolve Git Errors\n";;
+  *) printf "${cg}Usage: ./install.sh ${cb}[i|u|up]${cb}
+    i:${cg} Install
+    ${cb}u:${cg} Uninstall
+   ${cb}up:${cg} Update\n"
 esac
