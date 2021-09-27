d57d46a11ba120876b8d7f93f7e7a3dbe721627d
manas140 <manasbhosale.14@gmail.com>
2021-09-27 13:31:49 +0000

    run script & some test files

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ cb="\033[1;34m"
 printf "${cr}"
 case $1 in 
   u) for script in $(ls src/); do 
-    rm -r /usr/local/bin/$script && printf "${cg}[*] $script Uninstalled\n" || printf "${cr}[-] There Was An Error While Uninstalling $script\n"; 
+    sudo rm -r /usr/local/bin/$script && printf "${cg}[*] $script Uninstalled\n" || printf "${cr}[-] There Was An Error While Uninstalling $script\n"; 
   done;;
   i) for script in $(ls src/); do
-    cp src/$script /usr/local/bin/$script && printf "${cg}[*] $script Installed\n" || printf "${cr}[-] There Was An Error While Installing $script\n";
+    sudo cp src/$script /usr/local/bin/$script && printf "${cg}[*] $script Installed\n" || printf "${cr}[-] There Was An Error While Installing $script\n";
   done;;
   up) sh install.sh u
     printf "${cr}"
-    git pull && sh install.sh i || printf "${cr}Please Resolve Git Errors\n";;
+    git pull && sh install.sh i || printf "${cr}Error: Please Resolve Git Errors\n";;
   *) printf "${cg}Usage: ./install.sh ${cb}[i|u|up]${cb}
     i:${cg} Install
     ${cb}u:${cg} Uninstall
